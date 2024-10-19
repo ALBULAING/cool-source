c2a886acb08aed4db9ec1d2e5793b672db0c5c35
Aaaliang <152960807+ALBULAING@users.noreply.github.com>
2024-10-19 19:10:03 +0800

    Add files via upload

---
+++ uptime-status/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站服务状态监控平台',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2631068-c5d3500ed9a3711613eb8190',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://home.coolspace.us.kg/'
+    },
+    {
+      text: '博客',
+      url: 'https://cool.com.mp/'
+    },
+  ],
+};
